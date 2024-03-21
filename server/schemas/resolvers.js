4512f3c14c0a0a6c91d20e00ed9d1da3c6d8c95e
Conartisttt <connerwmar@gmail.com>
2024-03-20 21:46:25 -0400

    search functionality mvp

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -57,13 +57,13 @@ const resolvers = {
                     { _id: context.user._id },
                     {
                         $pull: {
-                            books: {bookId}
+                            books: { bookId }
                         },
                     },
-            { new: true }
+                    { new: true }
                 )
             }
-throw AuthenticationError;
+            throw AuthenticationError;
         },
     },
 };
