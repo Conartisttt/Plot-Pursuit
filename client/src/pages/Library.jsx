7d508727141bef81d9d5b63bcf498dd1ade15359
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-21 21:37:23 -0400

    readmecover fix and card display fixed

--- client/src/pages/Library.jsx
+++ client/src/pages/Library.jsx
@@ -11,28 +11,19 @@ const Library = () => {
   return (
     <div className="container">
       <h1 className="text-center">Library</h1>
-      <div className="row">
+      <div className="row row-cols-1 row-col-md-3 g-4">
         {data.me.books.map((book, i) => (
           <div key={i}>
-            <div key={book.bookId} className="col-md-3 mb-4">
-              <Link
-                to={`/Library/${book.bookId}`}
-                className="text-decoration-none"
-              >
-                <div className="card">
-                  <img
-                    src={book.image}
-                    className="card-img-top centered-image"
-                    alt={book.title}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{book.title}</h5>
-                    <p className="card-text">
-                      Author: {book.authors.join(", ")}
-                    </p>
-                  </div>
-                </div>
-              </Link>
+            <div key={book.bookId} className="col mb-4 libbooks">
+            <Link to={`/Library/${book.bookId}`} className='text-decoration-none'>
+            <div className="card" style={{width:"300px", marginLeft:"10px", marginRight:'10px'}}>
+              <img src={book.isRead ? book.image:'/readmecover.jpg'} className="card-img-top centered-image readme" alt={book.title} style={{height:'175px', width:'125px', paddingTop:'10px'}}/>
+              <div className="card-body">
+                <h5 className="card-title">{book.title}</h5>
+                <p className="card-text">Author: {book.authors.join(', ')}</p>
+              </div>
+            </div>
+            </Link>
             </div>
           </div>
         ))}
