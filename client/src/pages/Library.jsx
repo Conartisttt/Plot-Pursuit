cb86c055616254c6001bb6eaf24898c878270af3
Conartisttt <connerwmar@gmail.com>
2024-03-21 20:50:20 -0400

    single book page functionality working

--- client/src/pages/Library.jsx
+++ client/src/pages/Library.jsx
@@ -1,28 +1,39 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
 
 const Library = () => {
   const { loading, error, data } = useQuery(GET_ME);
-  
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
-  
+
   return (
     <div className="container">
       <h1 className="text-center">Library</h1>
       <div className="row">
-        {data.me.books.map(book => (
-          <div key={book.bookId} className="col-md-3 mb-4">
-            <Link to={`/Library/${book.bookId}`} className='text-decoration-none'>
-            <div className="card">
-              <img src={book.isRead ? book.image:'/readmecover.jpg'} className="card-img-top centered-image readme" alt={book.title} style={{height:'175px', width:'125px'}}/>
-              <div className="card-body">
-                <h5 className="card-title">{book.title}</h5>
-                <p className="card-text">Author: {book.authors.join(', ')}</p>
-              </div>
+        {data.me.books.map((book, i) => (
+          <div key={i}>
+            <div key={book.bookId} className="col-md-3 mb-4">
+              <Link
+                to={`/Library/${book.bookId}`}
+                className="text-decoration-none"
+              >
+                <div className="card">
+                  <img
+                    src={book.image}
+                    className="card-img-top centered-image"
+                    alt={book.title}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{book.title}</h5>
+                    <p className="card-text">
+                      Author: {book.authors.join(", ")}
+                    </p>
+                  </div>
+                </div>
+              </Link>
             </div>
-            </Link>
           </div>
         ))}
       </div>
@@ -31,4 +42,3 @@ const Library = () => {
 };
 
 export default Library;
-
