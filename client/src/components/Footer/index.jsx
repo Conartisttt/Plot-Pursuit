559c4728f4e8febe0408afc77fb0c8e46957775f
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-03-25 15:53:59 -0400

    added comments

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -14,12 +14,15 @@ const Footer = () => {
 
       }}
     >
+        {/* Container to hold footer content */}
       <div className="container text-left">
+         {/* Render 'Go Back' button only if not on the home page */}
         {location.pathname !== '/' && (
           <button className="btn btn-sm mb-3" onClick={() => navigate(-1)}>
             &larr; Go Back
           </button>
         )}
+        {/* Footer text */}
         <h4 style={{ fontSize: '12px' }}>
           &copy; {new Date().getFullYear()} - Plot Persuit Made with readers in mind           by: Daelyn Hiduchick, Connor Martin, and Shinayomi Ogunbayo
         </h4>
--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,13 +1,17 @@
 import { useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
+    // Get the error information from the route
   const error = useRouteError();
-  console.error(error);
+  console.error(error); // Log the error to the console
 
   return (
     <div id="error-page">
+        {/* Display a heading */}
       <h1>Oops!</h1>
+      {/* Display a message indicating an unexpected error */}
       <p>Sorry, an unexpected error has occurred.</p>
+       {/* Display the error message or status text */}
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
